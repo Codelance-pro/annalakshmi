bf5e36acd042628eedeb0504f02282aa21d601a6
suriya1331 <suriyanarayanan1331@gmail.com>
2026-08-08 00:18:45 +0530

    all new start

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+export const API_BASE = import.meta.env.VITE_API_URL || 'https://annalakshmi-backend-1.onrender.com'
 
 const API = axios.create({ baseURL: `${API_BASE}/api` })
 
